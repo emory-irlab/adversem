16452bdf6c8d01848e7d86f90c7bfced5073bcea
Kaustubh Dhole <kaustubhdhole@users.noreply.github.com>
2025-09-08 11:32:53 -0400

    Initial commit

---
+++ utils.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import socket
+
+load_dotenv(dotenv_path=f".env.local")
+
+hostname=socket.gethostname()
+IS_SERVER =  hostname.endswith('server') # if code is running on the server
+
+
+import json
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
